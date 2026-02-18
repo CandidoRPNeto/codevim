4741acad5b77c53fcabd19f5bc1e8b11dac237b4
CandidoRPNeto <candidoperalva.dev@gmail.com>
2026-02-18 20:22:44 -0300

    fix lsp problem

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,54 +29,86 @@ return {
         "rust_analyzer",
         "cssls",
         "html",
-      },
-      handlers = {
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
-        lua_ls = function()
-          require("lspconfig").lua_ls.setup({})
-        end,
-        ts_ls = function()
-          require("lspconfig").ts_ls.setup({
-            cmd = { "typescript-language-server", "--stdio" },
-            filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-          })
-        end,
-        pyright = function()
-          require("lspconfig").pyright.setup({
-            cmd = { "pyright-langserver", "--stdio" },
-            filetypes = { "python" },
-          })
-        end,
-        rust_analyzer = function()
-          require("lspconfig").rust_analyzer.setup({
-            cmd = { "rust-analyzer" },
-            filetypes = { "rust" },
-          })
-        end,
-        cssls = function()
-          require("lspconfig").cssls.setup({
-            cmd = { "vscode-css-language-server", "--stdio" },
-            filetypes = { "css", "scss" },
-          })
-        end,
-        html = function()
-          require("lspconfig").html.setup({
-            cmd = { "vscode-html-language-server", "--stdio" },
-            filetypes = { "html" },
-          })
-        end,
+        "intelephense",
       },
     })
-    -- Godot (gdscript) LSP — fora do Mason
-    local lspconfig = require("lspconfig")
-    local util = require("lspconfig.util")
 
-    lspconfig.gdscript.setup({
+    vim.lsp.config("*", {
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
+        signs = true,
+      },
+      on_attach = function(client, bufnr)
+        local function buf_set_keymap(...)
+          vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+        buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true, desc = "Go to definition" })
+        buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true, desc = "Hover documentation" })
+        buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true, desc = "Go to implementation" })
+        buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true, desc = "Go to references" })
+        buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true, desc = "Code actions" })
+        buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true, desc = "Rename" })
+      end,
+    })
+
+    vim.lsp.config("lua_ls", {
+      settings = {
+        Lua = {
+          runtime = { version = "LuaJIT" },
+          diagnostics = { globals = { "vim" } },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+          telemetry = { enable = false },
+        },
+      },
+    })
+
+    vim.lsp.config("ts_ls", {
+      cmd = { "typescript-language-server", "--stdio" },
+      filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+    })
+
+    vim.lsp.config("pyright", {
+      cmd = { "pyright-langserver", "--stdio" },
+      filetypes = { "python" },
+    })
+
+    vim.lsp.config("rust_analyzer", {
+      cmd = { "rust-analyzer" },
+      filetypes = { "rust" },
+    })
+
+    vim.lsp.config("cssls", {
+      cmd = { "vscode-css-language-server", "--stdio" },
+      filetypes = { "css", "scss" },
+    })
+
+    vim.lsp.config("html", {
+      cmd = { "vscode-html-language-server", "--stdio" },
+      filetypes = { "html" },
+    })
+
+    vim.lsp.config("intelephense", {
+      cmd = { "intelephense", "--stdio" },
+      filetypes = { "php" },
+      root_dir = vim.fs.root(0, { "composer.json", ".git" }),
+    })
+
+    vim.lsp.config("gdscript", {
       cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
       filetypes = { "gd", "gdscript", "gdscript3" },
-      root_dir = util.root_pattern("project.godot"),
+      root_dir = function(fname)
+        return vim.fs.root(fname, { "project.godot" })
+      end,
       autostart = true,
     })
 
@@ -88,26 +120,25 @@ return {
           .insert({
             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping
-                .complete(),
+            ["<C-Space>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.abort(),
             ["<CR>"] = cmp.mapping.confirm({ select = true }),
           }),
-      sources = cmp
-          .config.sources({ { name = "nvim_lsp" }, { name = "luasnip" }, }, { { name = "buffer" }, { name = "path" }, }),
+      sources = cmp.config.sources(
+        { { name = "nvim_lsp" }, { name = "luasnip" } },
+        { { name = "buffer" }, { name = "path" } }
+      ),
     })
-    cmp.setup.cmdline({ "/", "?" }, { mapping = cmp.mapping.preset.cmdline(), sources = { { name = "buffer" }, }, })
-    cmp.setup.cmdline(":",
-      {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({ { name = "path" }, },
-          { { name = "cmdline" }, }),
-      })
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = { { name = "buffer" } },
+    })
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources(
+        { { name = "path" } },
+        { { name = "cmdline" } }
+      ),
+    })
   end,
 }
